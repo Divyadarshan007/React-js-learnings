ee7174056189bdaa3f81b52fbc0b3b736ac618e9
divyadarshan007 <dasdivyadarshan@gmail.com>
2025-05-12 15:11:34 +0530

    react js learnings

---
+++ lecture-4-state/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  return (
+    <div className="container my-5">
+      <h1>{count}</h1>
+      <button className=" btn btn-primary" onClick={()=>{
+        setCount(count + 1);
+      }}>click me</button>
+    </div>
+  )
+}
+
+export default App
